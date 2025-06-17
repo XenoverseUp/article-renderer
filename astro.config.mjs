a86d51e521661d8b8c04de15ded106322f0b3b6d
XenoverseUp <xenoverseup@gmail.com>
2025-06-17 04:41:51 +0300

    feat: responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   prefetch: true,
   integrations: [],
+
   experimental: {
     responsiveImages: true,
   },
+
+  adapter: cloudflare(),
 });
